5dc592a17264a323aa7f7bafab4fb5b6811413a2
oglass <alexbirdand@gmail.com>
2024-07-02 23:20:52 -0400

    chore: setup reposilite publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,82 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "2.0.0"
+    id("io.github.goooler.shadow") version "8.1.7"
+
+    `maven-publish`
+
+    id("org.jetbrains.dokka") version "1.9.20"
 }
 
-group = "net.bladehunt"
-version = "1.0-SNAPSHOT"
+allprojects {
+    group = property("group") ?: "net.bladehunt"
+    version = property("version") ?: "0.1.0"
 
-repositories {
-    mavenCentral()
-    maven("https://mvn.bladehunt.net/releases")
+    repositories {
+        mavenCentral()
+
+        maven("https://jitpack.io")
+        maven("https://repo.spongepowered.org/maven")
+        maven("https://repo.velocitypowered.com/snapshots/")
+    }
+
+    tasks.withType<JavaCompile> {
+        sourceCompatibility = "21"
+        targetCompatibility = "21"
+    }
+
+    tasks.withType<KotlinCompile> {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_21)
+            compilerOptions { freeCompilerArgs.addAll("-Xinline-classes", "-Xcontext-receivers") }
+        }
+    }
+}
+
+subprojects {
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "maven-publish")
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
-    compileOnly("net.minestom:minestom-snapshots:73b308673b")
+    compileOnly("net.minestom", "minestom-snapshots", property("minestom.version") as String)
+
+    testImplementation("net.minestom", "minestom-snapshots", property("minestom.version") as String)
+    testImplementation("io.kotest:kotest-assertions-core:5.9.0")
+    testImplementation("io.kotest:kotest-runner-junit5:5.9.0")
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks.named<ShadowJar>("shadowJar") {
+    archiveBaseName.set("minigame-lib")
+    mergeServiceFiles()
+    minimize()
 }
-kotlin {
-    jvmToolchain(21)
+
+tasks.withType<Test> { useJUnitPlatform() }
+
+tasks.build { dependsOn("shadowJar") }
+
+java { withSourcesJar() }
+
+publishing {
+    publications {
+        create<MavenPublication>("library") {
+            from(components["java"])
+            artifactId = "minigame-lib"
+        }
+    }
+    repositories {
+        maven {
+            name = "releases"
+            url = uri("https://mvn.bladehunt.net/releases")
+            credentials(PasswordCredentials::class) {
+                username = System.getenv("MAVEN_NAME")
+                password = System.getenv("MAVEN_SECRET")
+            }
+            authentication { create<BasicAuthentication>("basic") }
+        }
+    }
 }
