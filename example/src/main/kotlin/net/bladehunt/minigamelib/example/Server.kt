7769003b1a8e5c99da1ca4eb899ed76ed9f00000
oglass <alexbirdand@gmail.com>
2024-07-03 00:57:14 -0400

    chore: update example

--- example/src/main/kotlin/net/bladehunt/minigamelib/example/Server.kt
+++ example/src/main/kotlin/net/bladehunt/minigamelib/example/Server.kt
@@ -3,29 +3,44 @@ package net.bladehunt.minigamelib.example
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import net.bladehunt.kotstom.CommandManager
 import net.bladehunt.kotstom.GlobalEventHandler
 import net.bladehunt.kotstom.InstanceManager
-import net.bladehunt.kotstom.dsl.builder
+import net.bladehunt.kotstom.dsl.kommand.kommand
 import net.bladehunt.kotstom.dsl.listen
 import net.minestom.server.MinecraftServer
+import net.minestom.server.coordinate.Pos
 import net.minestom.server.event.player.AsyncPlayerConfigurationEvent
 import net.minestom.server.event.player.PlayerSpawnEvent
+import net.minestom.server.instance.LightingChunk
+import net.minestom.server.instance.block.Block
+import net.minestom.server.utils.chunk.ChunkSupplier
 
 suspend fun main() =
     CoroutineScope(Dispatchers.Default).run {
         val server = MinecraftServer.init()
 
-        val instance = InstanceManager.createInstanceContainer()
+        val spawnInstance = InstanceManager.createInstanceContainer()
 
         GlobalEventHandler.listen<AsyncPlayerConfigurationEvent> { event ->
-            event.spawningInstance = instance
+            event.spawningInstance = spawnInstance
         }
-        val scope = ExampleGame.Scope(instance)
+
+        val gameInstance = InstanceManager.createInstanceContainer()
+        gameInstance.chunkSupplier = ChunkSupplier(::LightingChunk)
+        gameInstance.setGenerator { unit -> unit.modifier().fillHeight(0, 10, Block.STONE) }
+        gameInstance.eventNode().listen<PlayerSpawnEvent> {
+            it.player.teleport(Pos(0.5, 11.0, 0.5))
+        }
+
+        val scope = ExampleGame.Scope(gameInstance)
         launch { ExampleGame.start(scope) }
 
-        GlobalEventHandler.builder<PlayerSpawnEvent> {
-            asyncHandler { event -> scope.addPlayer(event.player) }
-        }
+        CommandManager.register(
+            kommand {
+                name = "join"
+                defaultExecutor { scope.addPlayer(player) }
+            })
 
         server.start("127.0.0.1", 25565)
     }
