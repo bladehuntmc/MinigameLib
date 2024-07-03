e692972c9bfd115afbc3894038792efcc0cfddc4
oglass <alexbirdand@gmail.com>
2024-07-03 19:22:49 -0400

    chore: update example server

--- example/src/main/kotlin/net/bladehunt/minigamelib/example/Server.kt
+++ example/src/main/kotlin/net/bladehunt/minigamelib/example/Server.kt
@@ -13,34 +13,48 @@ import net.minestom.server.MinecraftServer
 import net.minestom.server.coordinate.Pos
 import net.minestom.server.event.player.AsyncPlayerConfigurationEvent
 import net.minestom.server.event.player.PlayerSpawnEvent
+import net.minestom.server.instance.Instance
 import net.minestom.server.instance.LightingChunk
 import net.minestom.server.instance.block.Block
 import net.minestom.server.utils.chunk.ChunkSupplier
+
+lateinit var lobbyInstance: Instance
 
 suspend fun main() =
     CoroutineScope(Dispatchers.Default).run {
         val server = MinecraftServer.init()
 
-        val spawnInstance = InstanceManager.createInstanceContainer()
+        lobbyInstance =
+            InstanceManager.createInstanceContainer().apply {
+                chunkSupplier = ChunkSupplier(::LightingChunk)
+                setGenerator { unit -> unit.modifier().fillHeight(0, 10, Block.BRICKS) }
+            }
 
         GlobalEventHandler.listen<AsyncPlayerConfigurationEvent> { event ->
-            event.spawningInstance = spawnInstance
+            event.spawningInstance = lobbyInstance
+            event.player.respawnPoint = Pos(0.5, 11.0, 0.5)
         }
 
-        val gameInstance = InstanceManager.createInstanceContainer()
-        gameInstance.chunkSupplier = ChunkSupplier(::LightingChunk)
-        gameInstance.setGenerator { unit -> unit.modifier().fillHeight(0, 10, Block.STONE) }
-        gameInstance.eventNode().listen<PlayerSpawnEvent> {
-            it.player.teleport(Pos(0.5, 11.0, 0.5))
-        }
+        val gameInstance =
+            InstanceManager.createInstanceContainer().apply {
+                chunkSupplier = ChunkSupplier(::LightingChunk)
+                setGenerator { unit -> unit.modifier().fillHeight(0, 10, Block.STONE) }
+                eventNode().listen<PlayerSpawnEvent> { it.player.teleport(Pos(0.5, 11.0, 0.5)) }
+            }
 
-        val instance = SneakOffInstance(gameInstance)
-        launch { instance.start() }
+        val sneakOff = SneakOffInstance(gameInstance)
+        launch { sneakOff.start() }
 
         CommandManager.register(
             kommand {
                 name = "join"
-                defaultExecutor { instance.addPlayer(player) }
+                defaultExecutor { sneakOff.addPlayer(player) }
+            })
+
+        CommandManager.register(
+            kommand {
+                name = "lobby"
+                defaultExecutor { player.setInstance(lobbyInstance) }
             })
 
         server.start("127.0.0.1", 25565)
--- example/src/main/kotlin/net/bladehunt/minigamelib/example/sneakoff/SneakOff.kt
+++ example/src/main/kotlin/net/bladehunt/minigamelib/example/sneakoff/SneakOff.kt
@@ -5,15 +5,20 @@ import net.bladehunt.kotstom.dsl.listen
 import net.bladehunt.kotstom.extension.adventure.asComponent
 import net.bladehunt.kotstom.extension.adventure.color
 import net.bladehunt.kotstom.extension.adventure.plus
-import net.bladehunt.minigamelib.Game
+import net.bladehunt.minigamelib.InstancedGame
 import net.bladehunt.minigamelib.descriptor.GameDescriptor
 import net.bladehunt.minigamelib.dsl.element
 import net.bladehunt.minigamelib.dsl.gameDescriptor
 import net.bladehunt.minigamelib.element.countdown
+import net.bladehunt.minigamelib.example.lobbyInstance
 import net.kyori.adventure.text.format.NamedTextColor
+import net.minestom.server.entity.Player
 import net.minestom.server.event.player.PlayerStartSneakingEvent
+import net.minestom.server.instance.Instance
 
-object SneakOff : Game<SneakOffInstance>() {
+object SneakOff : InstancedGame<SneakOffInstance> {
+    override fun getFallback(instance: SneakOffInstance, player: Player): Instance = lobbyInstance
+
     override val descriptor: GameDescriptor<SneakOffInstance> = gameDescriptor {
         name = "Example Game"
 
@@ -22,10 +27,10 @@ object SneakOff : Game<SneakOffInstance>() {
             sendMessage("Starting game!".asComponent())
         }
         element {
-            sendMessage("Do stuff...".asComponent())
+            sendMessage("Sneak as many times as possible!".asComponent())
             elementEventNode.listen<PlayerStartSneakingEvent> {
                 it.player.sendMessage("You sneaked")
-                it.player.sneakCount += 1
+                it.player.sneakCount++
             }
 
             repeat(10) { i ->
@@ -34,9 +39,12 @@ object SneakOff : Game<SneakOffInstance>() {
             }
         }
         element {
-            sendMessage(
-                "Game Overview".color(NamedTextColor.YELLOW).appendNewline() +
-                    "Players: ${players.joinToString { "${it.username} - ${it.sneakCount}" }}")
+            var message = "<-- Game Overview -->".color(NamedTextColor.YELLOW)
+            players
+                .sortedByDescending { it.sneakCount }
+                .forEach { message = message.appendNewline() + "${it.username} - ${it.sneakCount}" }
+
+            sendMessage(message)
         }
     }
 }
